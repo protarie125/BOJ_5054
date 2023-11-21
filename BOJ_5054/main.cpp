8a5333b0fd8669f8585671640903a797883b76bd
protarie125 <protarie@gmail.com>
2023-11-21 20:14:40 +0900

    초기 커밋.

---
+++ BOJ_5054/main.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <bit>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while (0 < (t--)) {
+		int n;
+		cin >> n;
+
+		auto xs = vi(n);
+
+		auto minx = 100;
+		auto maxx = 0;
+		for (auto&& x : xs) {
+			cin >> x;
+
+			if (x < minx) {
+				minx = x;
+			}
+
+			if (maxx < x) {
+				maxx = x;
+			}
+		}
+
+		cout << (maxx - minx) * 2 << '\n';
+	}
+
+	return 0;
+}
